21493a0e9b0287459599c2e9799cdae48ea006eb
Santiago León <sleoncamacho@outlook.com>
2024-08-07 00:49:58 -0500

    Update index.js

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -31,22 +31,27 @@ const authServiceProxy = createProxyMiddleware({
     },
   });
 
-const chatServiceProxy = createProxyMiddleware({
-  target: process.env.CHAT_SERVICE_URL,
-  ws: true,
-  changeOrigin: true,
-  pathRewrite: { '^/chat': '' },
-});
-
-app.use('/auth', authServiceProxy);
-app.use('/inventory', inventoryServiceProxy);
-app.use('/chat', chatServiceProxy);
-
-app.get('/', (req, res) => {
-  res.send('API Gateway Running');
-});
-
-const PORT = process.env.PORT || 3010;
-app.listen(PORT, () => {
-  console.log(`API Gateway running on port ${PORT}`);
-});
+  const chatServiceProxy = createProxyMiddleware({
+    target: process.env.CHAT_SERVICE_URL,
+    ws: true,
+    changeOrigin: true,
+    pathRewrite: { '^/chat': '' },
+    onProxyReq: (proxyReq, req, res) => {
+      if (req.headers.authorization) {
+        proxyReq.setHeader('Authorization', req.headers.authorization);
+      }
+    },
+  });
+  
+  app.use('/auth', authServiceProxy);
+  app.use('/inventory', inventoryServiceProxy);
+  app.use('/chat', chatServiceProxy);
+  
+  app.get('/', (req, res) => {
+    res.send('API Gateway Running');
+  });
+  
+  const PORT = process.env.PORT || 3010;
+  app.listen(PORT, () => {
+    console.log(`API Gateway running on port ${PORT}`);
+  });
